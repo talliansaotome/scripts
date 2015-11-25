93e76aa1daf631d709941ba381db2601a90d1eec
Ben Miller <tallian.saotome@gmail.com>
2015-11-24 23:02:02 -0800

    adding serialcheck

---
+++ serialcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -sp 'Username: ' USER
+read -sp 'Password: ' PASSWORD
+
+while read HOST;do
+	ssh -t $USER@$HOST sho inv > $HOST.out
+	expect "password"
+	send "$PASSWORD\r"
+	cat $HOST.out |grep -i serial >> serials.list
+done < hostlist
