418924896073c7cac1f92fb3f9d67bc9dbb4d271
Ben Miller <tallian.saotome@gmail.com>
2015-11-24 23:14:08 -0800

    tweak expect, make work

--- serialcheck.sh
+++ serialcheck.sh
@@ -4,8 +4,8 @@ read -p 'Username: ' USER
 read -sp 'Password: ' PASSWORD
 
 while read HOST;do
-	ssh -t $USER@$HOST sho inv > $HOST.out
-	expect "password"
-	send "$PASSWORD\r"
+	spawn ssh -t $USER@$HOST sho inv > $HOST.out
+	expect \"password\"
+	send \"$PASSWORD\r\"
 	cat $HOST.out |grep -i serial >> serials.list
 done < hostlist
