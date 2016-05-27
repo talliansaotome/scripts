fd4327ffe49968fdaf0de11b5657933c2e1885d3
Ben Miller <tallian.saotome@gmail.com>
2016-05-26 23:08:46 -0500

    Update rmlargefile.sh
    added warnings before removal, warnings that file is not extremely large, remote functionality
    
    TODO: local or remote functionality, possibly getopts to override default search path, proper handling for choices other than 'y/n'

--- rmlargefile.sh
+++ rmlargefile.sh
@@ -6,22 +6,43 @@ SERVERNAME=$1
 LOGFILENAME=~/removedfiles.log
 
 ###Variable logic
-LARGESTSIZE=`find $WORKPATH -type f -exec du -ka {} + | sort -n | tail -1 | awk '{print $1}'`
-LARGESTNAME=`find $WORKPATH -type f -exec du -ka {} + | sort -n | tail -1 | awk '{print $2}'`
-VARLOGSIZE=`df -k $WORKPATH | tail -1 | awk '{print $2}'`
-VARLOGFS=`df -k $WORKPATH | tail -1 | awk '{print $6}'`
+LARGESTSIZE=`ssh $SERVERNAME "find $WORKPATH -type f -exec du -ka {} + " | sort -n | tail -1 | awk '{print $1}' | awk -F\\\ '{print $1}'`
+LARGESTNAME=`ssh $SERVERNAME "find $WORKPATH -type f -exec du -ka {} + " | sort -n | tail -1 | awk '{print $2}' | awk -F\\\ '{print $1}'`
+VARLOGSIZE=`ssh $SERVERNAME "df -k $WORKPATH" | tail -1 | awk '{print $2}' | awk -F\\\ '{print $1}'`
+VARLOGFS=`ssh $SERVERNAME "df -k $WORKPATH" | tail -1 | awk '{print $6}' | awk -F\\\ '{print $1}'`
 PERCENTUSED=$((100*$LARGESTSIZE/$VARLOGSIZE))
  
 ### Informational output
 echo "$LARGESTNAME  is $LARGESTSIZE k"
-echo "$WORKPATH is $VARLOGSIZE k on $VARLOGFS"
+#echo "$WORKPATH is $VARLOGSIZE k on $VARLOGFS"
 echo "$LARGESTNAME is taking up $PERCENTUSED % of $VARLOGFS"
 
-### Verification Prompt
-read -p 'Press Ctrl-C to cancel or Enter to continue'
+### Is file large enough? Do we still want to delete it?
+if [ "$PERCENTUSED" -lt "50" ]; then
+echo "No single file is not using more than 50% of the filesystem."
+echo "$LARGESTNAME is the largest, using $PERCENTUSED of $VARLOGFS."
+read -p "Remove $LARGESTNAME? (y/n}: " YN
+if [ "$YN" == "y" ]; then
+ssh -t $SERVERNAME rm -Iv $LARGESTNAME
+echo "Other large files may remain, investigate."
+echo "$USER removed $LARGESTNAME on $(date). Other large files may remain." >> $LOGFILENAME
+exit 0
+elif [ "$YN" == "n" ]; then
+echo "Nothing removed."
+exit 1
+fi
+fi
 
-### Remove large file
-rm -Iv $LARGESTNAME
+### Verification Prompt
+read -p "Remove $LARGESTNAME? (y/n}" YN
+if [ "$YN" == "y" ]; then
+ssh -t $SERVERNAME rm -Iv $LARGESTNAME
+echo "$USER removed $LARGESTNAME on $(date)." >> $LOGFILENAME
+exit 0
+elif [ "$YN" == "n" ]; then
+echo "Nothing removed."
+exit 1
+fi
 
 ### Log removal
-echo "$USERNAME removed $LARGESTNAME on $(date)." > $LOGFILENAME
+echo "$USER removed $LARGESTNAME on $(date)." >> $LOGFILENAME
