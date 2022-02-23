c933008e40aaa7279f39ecbd3b8f3123d29672c6
Lily Miller <tallian.saotome@gmail.com>
2022-02-22 21:37:28 -0700

    Update atlassianupgrade.sh

--- atlassianupgrade.sh
+++ atlassianupgrade.sh
@@ -5,12 +5,14 @@
 ###Put ourselves in a screen session
 if [[ $STY = "" ]]; then
 	echo "Running ourself in a screen session."
-	screen -S confupgrade "$0"
+	screen -S $(basename $0) -L "$0"
+	mv screenlog.? $(basename $0).log
 	exit
 fi
 
+##Collect info we will need
 
-## What are we working with?
+# What are we working with?
 echo "Detecting which app we are using..."
 if [[ -f data/confluence.cfg.xml ]] ; then
 	APP=confluence
@@ -27,27 +29,22 @@ elif [[ -f data/dbconfig.xml ]] ; then
 	echo "We are working with Jira."
 	FILEOWNER=$(stat -c '%U' data/dbconfig.xml)
 	echo "Files are owned by $FILEOWNER"
-	SHAREDOPTS=
+	SHAREDOPTS=--exclude={data/attachments,analytics-logs,backup,caches,*.bak,tmp,temp,export,import,log,logs,plugins/.??*,CACHECLEAR*,cache-clear*,cacheclear*}
 else
 	echo "No app install detected."
 	exit 1
 fi
 
-
-
-VERSION=
-SERVICENAME=
-TICKET=
-
-
-read -p "Ticket Number? " TICKET
-read -p "Target Version? " VERSION
+# Get database info needed
+DATABASECONNECTION=$(awk -F '[<>]' '/url/{print $3}' $CONFIGFILE)
+DATABASE=$(awk -F/ '{print $4}' <<< "$DATABASECONNECTION")
+echo "Using $DATABASE"
 echo ""
 
 ## Parse the service name, prompt if this fails
 echo "Finding active service name..."
-if [[ $(systemctl|grep j2ee | wc -l) -eq 1 ]] ; then
-	SERVICENAME=$(systemctl|grep j2ee|awk '{print $1}')
+if [[ $(systemctl|grep -c j2ee) -eq 1 ]] ; then
+	SERVICENAME=$(systemctl -al|grep j2ee|awk '{print $1}')
 	echo "Using $SERVICENAME as the active service."
 else
 	echo "Unable to automatically determine service, what should we be using?"
@@ -58,19 +55,18 @@ else
 	echo ""
 fi
 
-
-
-##Collect info we will need
-
-DATABASECONNECTION=$(awk -F '[<>]' '/url/{print $3}' $CONFIGFILE)
-DATABASE=$(awk -F/ '{print $4}' <<< "$DATABASECONNECTION")
-echo "Using $DATABASE"
+# Get other info we will need.
+read -p "Ticket Number? " TICKET
+read -p "Target Version? " VERSION
 echo ""
 
 
+
+## Whats the job
 echo "1) Prep"
 echo "2) Run the Sched"
 echo "3) ROLLBACK"
+echo "X) Exit. or any key really."
 echo ""
 read -n 1 -p 'Well? ' CHOICE
 echo ""
@@ -79,8 +75,8 @@ echo ""
 if [ "$CHOICE" = "1" ] ; then
 ###PREP
 
-
-### Check for enough space to do the work
+## Check for enough space to do the work
+echo "Checking for enough disk space for the upgrade..."
 DATADIRSIZE=$(du -sk data/ "$SHAREDOPTS"|awk {'print $1'})
 FREESPACE=$(df -k . --output=avail|tail -1)
 
@@ -91,7 +87,6 @@ else
 	echo "Enough space found, proceeding."
 fi
 
-
 ##Set up the files
 
 # Clone the data, no attachments
@@ -99,16 +94,14 @@ echo "Copying data dir"
 time rsync -aHS "$SHAREDOPTS" data/ data-"$VERSION"/ || exit 1
 ln -s data-"$VERSION" next.data || echo "Linking the next data/ dir failed."
 
-
 # Get the application
 echo "Fetching and installing the app"
 wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-"$VERSION".tar.gz || exit 1
 tar xzf atlassian-confluence-"$VERSION".tar.gz || exit 1
-ln -s atlassian-confluence-"$VERSION" next || echo "Linking the next app dir failed"
-
-
+ln -s atlassian-confluence-"$VERSION" next || echo "Linking the next current/ dir failed"
 
 ## The fancy diffs
+echo "Checking for changes to config files..."
 if [[ $APP = confluence ]] ; then
 	for FILEPAIR in "data/confluence.cfg.xml next.data/confluence.cfg.xml" "current/bin/setenv.sh next/bin/setenv.sh" "current/conf/server.xml next/conf/server.xml" "current/conf/web.xml next/conf/web.xml" "current/confluence/WEB-INF/classes/confluence-init.properties next/confluence/WEB-INF/classes/confluence-init.properties" "current/confluence/WEB-INF/classes/seraph-config.xml next/confluence/WEB-INF/classes/seraph-config.xml" "current/confluence/WEB-INF/classes/crowd.properties next/confluence/WEB-INF/classes/crowd.properties" "current/confluence/WEB-INF/classes/okta-config-confluence.xml next/confluence/WEB-INF/classes/okta-config-confluence.xml"
 		do 
@@ -167,31 +160,27 @@ elif [[ $APP = jira ]] ; then
 	done
 fi
 
-
-
-
-### Fix Permissions
+## Fix Permissions
+echo "Setting permissions..."
 chown -R root:root next/ && chown -R "$FILEOWNER". next/{conf,logs,temp,webapps,work} && chown -R "$FILEOWNER". next.data/ || exit 1
 echo "Permissions set"
 
-
-
-# This part of the guide never found anything in my experience, putting in anyway
+## This part of the guide never found anything in my experience, putting in anyway
 if [[ $APP = confluence ]] ; then
 	echo "Copying okta"
 	cp -vn current/confluence/WEB-INF/lib/okta-confluence-*.jar next/confluence/WEB-INF/lib/
 elif [[ $APP = jira ]] ; then
-	echo "checking for okta. deal with it if you find anything"
+	echo "Checking for okta. Deal with it if you find anything."
 	find current/ | grep okta
+	echo ""
+	echo ""
 	echo "Make sure the atlassian recommended settings are in..."
 	grep -xF 'upgrade.reindex.allowed=false' data/jira-config.properties || echo 'upgrade.reindex.allowed=false' >> data/jira-config.properties
 	grep -xF 'jira.autoexport=false' data/jira-config.properties || echo 'jira.autoexport=false' >> data/jira-config.properties
 fi
 
-
 echo "Prep completed!"
 exit
-
 
 elif [ "$CHOICE" = "2" ] ; then
 
@@ -221,10 +210,8 @@ else
 	read -p "Press any key to resume ..."
 fi
 
-
 ## Start and watch
 systemctl start "$SERVICENAME" && tail -F $APPLOG
-
 exit
 
 elif [ "$CHOICE" = "3" ] ; then
@@ -233,13 +220,11 @@ elif [ "$CHOICE" = "3" ] ; then
 
 systemctl stop "$SERVICENAME"
 
-
 ## Reimport database
 
 # Collect database creds
 DATABASEUSERNAME=$(awk -F '[<>]' '/username/{print $3}' $CONFIGFILE)
 DATABASEPASSWORD=$(awk -F '[<>]' '/password/{print $3}' $CONFIGFILE)
-
 
 ## check if database is local
 if [[ "$DATABASECONNECTION" == *"localhost"* ]]; then
@@ -266,4 +251,7 @@ mv prev current && mv prev.data data || exit 1
 systemctl start "$SERVICENAME" && tail -F $APPLOG
 
 exit
+else
+	echo "Please make a valid choice next time!"
+	exit 1
 fi
